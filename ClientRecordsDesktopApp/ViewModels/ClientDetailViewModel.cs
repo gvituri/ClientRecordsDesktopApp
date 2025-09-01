17a6f88d0202a52f6b6e46c8f646b34b7a8a69e3
Gustavo Vituri <gustavovituri@gmail.com>
2025-09-01 16:21:20 -0300

    add mvvm community toolkit

--- ClientRecordsDesktopApp/ViewModels/ClientDetailViewModel.cs
+++ ClientRecordsDesktopApp/ViewModels/ClientDetailViewModel.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ClientRecordsDesktopApp.ViewModels {
-    public partial  class ClientDetailViewModel {
+    public partial  class ClientDetailViewModel : ObservableObject {
     }
 }
