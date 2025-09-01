877353aa7f8c0faf86a6acdd579fb7f27e6dd5f0
Gustavo Vituri <gustavovituri@gmail.com>
2025-09-01 15:55:15 -0300

    Adicionar arquivos de projeto.

---
+++ ClientRecordsDesktopApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ClientRecordsDesktopApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
